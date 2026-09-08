617affcd041c4a759898a130f012e5ea8fdca650
Joao Matos <joao@tritao.eu>
2026-09-08 14:52:37 +0100

    Use unified Haxeon compiler CLI

--- scripts/haxeon-compile.sh
+++ scripts/haxeon-compile.sh
@@ -5,4 +5,4 @@ root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 haxeon_root=${HAXEON_ROOT:-"$root_dir/../realtime-haxe"}
 
 exec "$haxeon_root/.tools/haxe/haxe" --cwd "$haxeon_root" -cp src \
-	--run compiler.tools.BootstrapCompiler "$@"
+	--run compiler.tools.HaxeonCompiler "$@"
